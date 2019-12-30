486e2b69616fea23b2cb3673525451611b66b4d2
Kennedy Njeri <mistakenz123@gmail.com>
2019-12-30 18:10:50 +0300

    using yargs for terminal

--- app.js
+++ app.js
@@ -8,12 +8,15 @@
 //
 // console.log(add(5, 3))
 //const validator = require('validator')
+//const chalk = require('chalk');
+//const yargs = require('yargs')
+const yargs = require('yargs')
 const getNotes = require('./notes')
-const chalk = require('chalk');
 
-const error = chalk.bold.red;
-const success = chalk.bold.green.inverse;
-const warning = chalk.keyword('orange');
+
+// const error = chalk.bold.red;
+// const success = chalk.bold.green.inverse;
+// const warning = chalk.keyword('orange');
 
 
 // or const msg = getNotes()
@@ -21,9 +24,42 @@ const warning = chalk.keyword('orange');
 // console.log(msg)
 
 
-console.log(getNotes("Cool"))
-//console.log(chalk.blue('Hello world!'));
-//console.log(validator.isEmail('mistakenz@ymail.com'))
-console.log(error('Error warning!'));
-console.log(warning('Warning!'))
-console.log(success('Success!'))
+// console.log(getNotes("Cool"))
+// //console.log(chalk.blue('Hello world!'));
+// //console.log(validator.isEmail('mistakenz@ymail.com'))
+// console.log(error('Error warning!'));
+// console.log(warning('Warning!'))
+// console.log(success('Success!'))
+
+//console.log(process.argv)
+
+//argv- argument vector
+//const command = process.argv[2]
+
+// if (command == 'add') {
+//     console.log('Adding note')
+// }
+
+// customize yargs version
+yargs.version('1.1.0')
+
+//Create add Command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log("Adding a new note!")
+    }
+})
+
+// Create a remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a new note',
+    handler: function () {
+        console.log("Removing the note!")
+    }
+})
+
+
+console.log(yargs.argv)
